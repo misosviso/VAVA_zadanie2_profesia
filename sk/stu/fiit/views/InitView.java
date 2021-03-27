3ef67f7c64b8a509a9605953ce7ab4f7a9c678f3
misosviso <56072239+misosviso@users.noreply.github.com>
2021-03-27 10:22:02 +0100

    darknimbus lookandfeel

--- sk/stu/fiit/views/InitView.java
+++ sk/stu/fiit/views/InitView.java
@@ -5,15 +5,14 @@
  */
 package sk.stu.fiit.views;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import java.awt.Color;
 import javax.swing.UIManager;
 
 /**
  *
  * @author Admin
  */
-public class InitView extends javax.swing.JFrame {
+public class InitView extends javax.swing.JFrame{
 
     /**
      * Creates new form InitView
@@ -35,9 +34,9 @@ public class InitView extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jDesktopPane1 = new javax.swing.JDesktopPane();
         jToolBar1 = new javax.swing.JToolBar();
-        jButton3 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        companiesBtn = new javax.swing.JButton();
+        specialistsBtn = new javax.swing.JButton();
+        jobsBtn = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         jButton2.setText("jButton2");
@@ -47,40 +46,58 @@ public class InitView extends javax.swing.JFrame {
         jDesktopPane1.setAutoscrolls(true);
         jDesktopPane1.setLayout(new net.miginfocom.swing.MigLayout());
 
+        jToolBar1.setBackground(new java.awt.Color(255, 0, 0));
         jToolBar1.setFloatable(false);
         jToolBar1.setOrientation(javax.swing.SwingConstants.VERTICAL);
         jToolBar1.setRollover(true);
-        jToolBar1.setPreferredSize(new java.awt.Dimension(250, 125));
+        jToolBar1.setPreferredSize(new java.awt.Dimension(250, 100));
 
-        jButton3.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
-        jButton3.setText("Spoločnosti");
-        jButton3.setFocusable(false);
-        jButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jToolBar1.add(jButton3);
-
-        jButton1.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
-        jButton1.setText("IT Freelanceri");
-        jButton1.setFocusable(false);
-        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jToolBar1.add(jButton1);
-
-        jButton4.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
-        jButton4.setText("Pracovné ponuky");
-        jButton4.setFocusable(false);
-        jButton4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+        companiesBtn.setBackground(new java.awt.Color(255, 0, 51));
+        companiesBtn.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
+        companiesBtn.setForeground(new java.awt.Color(255, 255, 255));
+        companiesBtn.setText("Spoločnosti");
+        companiesBtn.setFocusable(false);
+        companiesBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        companiesBtn.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        companiesBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                companiesBtnMouseReleased(evt);
             }
         });
-        jToolBar1.add(jButton4);
+        jToolBar1.add(companiesBtn);
+
+        specialistsBtn.setBackground(new java.awt.Color(255, 0, 51));
+        specialistsBtn.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
+        specialistsBtn.setForeground(new java.awt.Color(255, 255, 255));
+        specialistsBtn.setText("IT Freelanceri");
+        specialistsBtn.setFocusable(false);
+        specialistsBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        specialistsBtn.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        specialistsBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                specialistsBtnMouseReleased(evt);
+            }
+        });
+        jToolBar1.add(specialistsBtn);
+
+        jobsBtn.setBackground(new java.awt.Color(255, 0, 51));
+        jobsBtn.setFont(new java.awt.Font("Segoe WP", 1, 18)); // NOI18N
+        jobsBtn.setForeground(new java.awt.Color(255, 255, 255));
+        jobsBtn.setText("Pracovné ponuky");
+        jobsBtn.setFocusable(false);
+        jobsBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jobsBtn.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jobsBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jobsBtnMouseReleased(evt);
+            }
+        });
+        jToolBar1.add(jobsBtn);
 
         jDesktopPane1.add(jToolBar1);
 
         jLabel1.setFont(new java.awt.Font("Segoe WP", 1, 22)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel1.setText("IT Profesia");
         jDesktopPane1.add(jLabel1);
@@ -99,9 +116,23 @@ public class InitView extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void companiesBtnMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_companiesBtnMouseReleased
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
+        new EmployersView().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_companiesBtnMouseReleased
+
+    private void specialistsBtnMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_specialistsBtnMouseReleased
+        // TODO add your handling code here:
+        new SpecialistsView().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_specialistsBtnMouseReleased
+
+    private void jobsBtnMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jobsBtnMouseReleased
+        // TODO add your handling code here:
+        new JobsView().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jobsBtnMouseReleased
 
     /**
      * @param args the command line arguments
@@ -112,8 +143,23 @@ public class InitView extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        UIManager.put( "control", new Color( 128, 128, 128) );
+        UIManager.put( "info", new Color(128,128,128) );
+        UIManager.put( "nimbusBase", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusAlertYellow", new Color( 248, 187, 0) );
+        UIManager.put( "nimbusDisabledText", new Color( 128, 128, 128) );
+        UIManager.put( "nimbusFocus", new Color(115,164,209) );
+        UIManager.put( "nimbusGreen", new Color(176,179,50) );
+        UIManager.put( "nimbusInfoBlue", new Color( 66, 139, 221) );
+        UIManager.put( "nimbusLightBackground", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusOrange", new Color(191,98,4) );
+        UIManager.put( "nimbusRed", new Color(169,46,34) );
+        UIManager.put( "nimbusSelectedText", new Color( 255, 255, 255) );
+        UIManager.put( "nimbusSelectionBackground", new Color( 104, 93, 156) );
+        UIManager.put( "text", new Color( 230, 230, 230) );
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                System.out.println(info.getName());
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
@@ -129,7 +175,6 @@ public class InitView extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(InitView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -139,12 +184,12 @@ public class InitView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton companiesBtn;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JButton jobsBtn;
+    private javax.swing.JButton specialistsBtn;
     // End of variables declaration//GEN-END:variables
 }
